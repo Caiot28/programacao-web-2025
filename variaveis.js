793361f6329b23be8e8178aee9595b473d94fbed
Caiot28 <caiobordinteles@gmail.com>
2025-02-28 10:36:09 -0300

    Primeira aula

---
+++ variaveis.js
@@ -0,0 +1,22 @@
+const n = 1;
+
+console.log("n = "+n);
+
+let j = n + 9;
+
+console.log("j = "+j);
+
+let nome = "Caio";
+let telefone = "1234";
+console.log(`Nome: ${nome}  Telefone: ${telefone}`);
+
+let pi = 3.14;
+console.log("pi = "+pi);
+
+let b = true;
+
+console.log(`b = ${b}`);
+
+let c;
+
+console.log(`c = ${c}`);

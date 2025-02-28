da1fbaaa52a78002141e71bbe014e30393df56f4
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2025-02-28 10:49:21 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,11 @@
+let a = 20;
+let b = 4;
+let w = a + b;
+let x = a - b;
+let y = a * b;
+let z = a / b;
+
+console.log(`Resultado da operacao entre ${a} + ${b} = ${w}`);
+console.log(`Resultado da operacao entre ${a} - ${b} = ${w}`);
+console.log(`Resultado da operacao entre ${a} * ${b} = ${w}`);
+console.log(`Resultado da operacao entre ${a} / ${b} = ${w}`);

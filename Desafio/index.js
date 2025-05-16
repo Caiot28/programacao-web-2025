1f4aa65d863add10eb09f5fe73e405f567dfa10f
Caiot28 <caiobordinteles@gmail.com>
2025-05-16 09:50:46 -0300

    Atualiza

--- Desafio/index.js
+++ Desafio/index.js
@@ -1,14 +1,38 @@
-const express = require('express')
-const mustacheExpress = require('mustache-express')
+const express = require('express');
+const mustacheExpress = require('mustache-express');
 const app = express()
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res)=>{
     
-    res.render('index.html')
+    res.render('index.html');
+});
+
+app.post('/agendar_consulta', (req, res)=>{
+
+    let dados = req.body;
+    let erro = false;
+    let campos_invalidos = [];
+
+    if(dados.inputNome.length == 0){
+        erro = true;
+        campos_invalidos.push("inputNome");
+    }
+    if(dados.inputSobrenome.length == 0){
+        erro = true;
+        campos_invalidos.push("inputSobrenome");
+    }
+    if(dados.inputCPF.length == 0){
+        erro = true;
+        campos_invalidos.push("inputCPF");
+    }
+    console.log(req.body)
+
+    res.render('index.html', (erro, campos_invalidos, dados));
 });
 
 const PORT = 8080;

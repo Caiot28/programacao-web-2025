d68d8517235528ae93287d94f068890d717eb908
Caiot28 <caiobordinteles@gmail.com>
2025-05-23 09:04:00 -0300

    Modulariza

--- Desafio/controllers/agendamentoController.js
+++ Desafio/controllers/agendamentoController.js
@@ -3,6 +3,60 @@ function getIndexView(req, res){
     res.render('index.html');
 }
 
+function postAgendarConsulta(req, res){
+    let dados = req.body;
+    let erro = false;
+    let campos_invalidos = [];
+
+    if(dados.inputNome.length == 0){
+        erro = true;
+        campos_invalidos.push("Nome");
+    }
+    if(dados.inputSobrenome.length == 0){
+        erro = true;
+        campos_invalidos.push("Sobrenome");
+    }
+    if(dados.inputCPF.length == 0){
+        erro = true;
+        campos_invalidos.push("CPF");
+    }
+    if(dados.inputTelefone.length == 0){
+        erro = true;
+        campos_invalidos.push("Telefone");
+    }
+    if(dados.inputCEP.length == 0){
+        erro = true;
+        campos_invalidos.push("CEP");
+    }
+    if(dados.inputEndereco.length == 0){
+        erro = true;
+        campos_invalidos.push("Endereco");
+    }
+    if(dados.inputDataNasc.length == 0){
+        erro = true;
+        campos_invalidos.push("Data de Nascimento");
+    }
+
+    if(dados.inputClinica == null){
+        erro = true;
+        campos_invalidos.push("Clinica");
+    }
+    if(dados.inputHora.length == 0){
+        erro = true;
+        campos_invalidos.push("Hora");
+    }
+    if(dados.inputData.length == 0){
+        erro = true;
+        campos_invalidos.push("Data");
+    }
+    if(dados.inputEspecialidade == null){
+        erro = true;
+        campos_invalidos.push("Especialidade");
+    }
+    res.render('index.html', {erro, campos_invalidos, dados});
+}
+
 module.exports = {
-    getIndexView
+    getIndexView,
+    postAgendarConsulta
 }
--- Desafio/index.js
+++ Desafio/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-const { getIndexView } = require('./controllers/agendamentoController');
 const app = express()
 
 app.engine('html', mustacheExpress());
@@ -8,63 +7,8 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use(express.urlencoded({extended: true}));
 
-const agendamentoController = require('./controllers/agendamentoController')
-app.get('/', agendamentoController.getIndexView);
-
-app.post('/agendar_consulta', (req, res)=>{
-
-    let dados = req.body;
-    let erro = false;
-    let campos_invalidos = [];
-
-    if(dados.inputNome.length == 0){
-        erro = true;
-        campos_invalidos.push("Nome");
-    }
-    if(dados.inputSobrenome.length == 0){
-        erro = true;
-        campos_invalidos.push("Sobrenome");
-    }
-    if(dados.inputCPF.length == 0){
-        erro = true;
-        campos_invalidos.push("CPF");
-    }
-    if(dados.inputTelefone.length == 0){
-        erro = true;
-        campos_invalidos.push("Telefone");
-    }
-    if(dados.inputCEP.length == 0){
-        erro = true;
-        campos_invalidos.push("CEP");
-    }
-    if(dados.inputEndereco.length == 0){
-        erro = true;
-        campos_invalidos.push("Endereco");
-    }
-    if(dados.inputDataNasc.length == 0){
-        erro = true;
-        campos_invalidos.push("Data de Nascimento");
-    }
-
-    if(dados.inputClinica == null){
-        erro = true;
-        campos_invalidos.push("Clinica");
-    }
-    if(dados.inputHora.length == 0){
-        erro = true;
-        campos_invalidos.push("Hora");
-    }
-    if(dados.inputData.length == 0){
-        erro = true;
-        campos_invalidos.push("Data");
-    }
-    if(dados.inputEspecialidade == null){
-        erro = true;
-        campos_invalidos.push("Especialidade");
-    }
-    
-    res.render('index.html', {erro, campos_invalidos, dados});
-});
+const agendamentoRouter = require('./routers/agendamentoRouter');
+app.use('/', agendamentoRouter)
 
 const PORT = 8080;
 app.listen(PORT, ()=>{
